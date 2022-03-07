b8c1448647ad980cf7e4b35a8f46233c18183d30
unknown <manuelpazpintor@gamil.com>
2022-03-07 14:59:35 +0100

    commit

--- ClienteAngular/angular-cliente/src/app/juego-detail/juego-detail.component.ts
+++ ClienteAngular/angular-cliente/src/app/juego-detail/juego-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit ,Input} from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Juego } from '../intefaces/juego';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
@@ -8,18 +8,20 @@ import { Sede } from '../intefaces/sede';
 @Component({
   selector: 'app-juego-detail',
   templateUrl: './juego-detail.component.html',
-  styleUrls: ['./juego-detail.component.css']
+  styleUrls: ['./juego-detail.component.css'],
 })
 export class JuegoDetailComponent implements OnInit {
   sedes!: Sede[];
-  nombre_ciudad:string;
+  nombre_ciudad: string;
   selectedSede: Sede;
-  ciudad_añadir:string;
-  ocultar_edicion=false;
-  constructor(private route: ActivatedRoute,
+  ciudad_añadir: string;
+  ocultar_edicion = false;
+  constructor(
+    private route: ActivatedRoute,
     private juegoService: JuegoService,
     private location: Location,
-    private messageService: MessageService) { }
+    private messageService: MessageService
+  ) {}
 
   ngOnInit(): void {
     this.getSedes();
@@ -27,25 +29,22 @@ export class JuegoDetailComponent implements OnInit {
   getSedes(): void {
     const id = +this.route.snapshot.paramMap.get('id_ciudad');
     const tipo = this.route.snapshot.paramMap.get('tipo');
-    this.nombre_ciudad= this.route.snapshot.paramMap.get('nombre_ciudad');
-    this.juegoService.getJuego(id,tipo).subscribe(juegos=>this.sedes=juegos);
-    this.ocultar_edicion=false;
-    
+    this.nombre_ciudad = this.route.snapshot.paramMap.get('nombre_ciudad');
+    this.juegoService
+      .getJuego(id, tipo)
+      .subscribe((juegos) => (this.sedes = juegos));
+    this.ocultar_edicion = false;
   }
   onSelect(hero: Sede): void {
     this.selectedSede = hero;
-    this.ciudad_añadir=null;
-    this.ocultar_edicion=false;
+    this.ciudad_añadir = null;
+    this.ocultar_edicion = false;
   }
   onSelect1(): void {
-    this.ciudad_añadir=this.nombre_ciudad;
-    this.ocultar_edicion=true;
- 
-    
+    this.ciudad_añadir = this.nombre_ciudad;
+    this.ocultar_edicion = true;
   }
   goBack(): void {
     this.location.back();
   }
-
-
 }
