53bd340075db0e49ffa7af440a9851b7889b35d0
unknown <manuelpazpintor@gamil.com>
2022-03-14 11:33:35 +0100

    pruebas

--- Servidor/src/test/java/com/example/demorest/services/JuegosServiceDeleteTest.java
+++ Servidor/src/test/java/com/example/demorest/services/JuegosServiceDeleteTest.java
@@ -3,6 +3,8 @@ package com.example.demorest.services;
 import com.example.demorest.dtos.JuegosDTO;
 import com.example.demorest.entities.JJOO;
 import com.example.demorest.repositories.JJOORepository;
+import org.aspectj.lang.annotation.Before;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -12,6 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -24,11 +27,24 @@ class JuegosServiceDeleteTest {
     JuegosService jjoo;
     @Autowired
     JJOORepository juegosRepository;
+    private  ArrayList<JuegosDTO> sedes;
+    private  ArrayList<Integer> numVeces;
+    @BeforeEach
+    public void init() {
+        this.sedes=new ArrayList();
+        this.numVeces=new ArrayList();
+        JuegosDTO sede1 = new JuegosDTO(1992, "VERANO");
+        sedes.add(sede1);
+        sede1 = new JuegosDTO(1992, "VERANO");
+        sedes.add(sede1);
+        numVeces.add(3);
+        numVeces.add(7);
 
 
+    }
     @DisplayName("Eliminar juegos")
     @ParameterizedTest(name = "Sede   a eliminar año:{0} ,tipo:{1}, id_ciudad:{2}")
-    @CsvSource({"1992,VERANO,3", "1924,INVIERNO,7"})
+    @CsvSource({"1992,1992,3", "1924,INVIERNO,7"})
     public void eliminarJuegos(int año, String tipo, int id_ciudad) {
 
         JuegosDTO sede = new JuegosDTO(año, tipo);
