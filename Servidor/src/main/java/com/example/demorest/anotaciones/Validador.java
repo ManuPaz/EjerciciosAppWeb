cb483129e008cbbb25505e352ee1274c81015efe
ManuPaz <64200507+ManuPaz@users.noreply.github.com>
2022-06-21 13:10:44 +0200

    Anotaciones, procesamiento con API Reflection

--- Servidor/src/main/java/com/example/demorest/anotaciones/Validador.java
+++ Servidor/src/main/java/com/example/demorest/anotaciones/Validador.java
@@ -1,28 +1,27 @@
 package com.example.demorest.anotaciones;
+
 import org.springframework.dao.DataIntegrityViolationException;
-import java.lang.reflect.Type;
+
 import java.util.Optional;
 
 public class Validador {
     private static final String THROWING_EXCEPTION_FOR_DEMOING_ROLLBACK = "Throwing exception for demoing Rollback!!!";
 
     /**
-     *
-     *
-     * @param opt Optional del que se quiere obtener el objeto
+     * @param opt   Optional del que se quiere obtener el objeto
      * @param clase Clase del objeto que deberia estar encapsulado en el Optional. Se pasa en un argumento separado porque
      *              si Optional no contiene un objeto no se puede extraer la clase
-     * @throws DataIntegrityViolationException
      * @return
+     * @throws DataIntegrityViolationException
      */
-    public static Object procesarOptional(Optional opt,Class clase) {
+    public static Object procesarOptional(Optional opt, Class clase) {
         final Entidad entidad = (Entidad) clase.getAnnotation(Entidad.class);
         if (entidad != null && entidad.obligatorio() && !opt.isPresent()) {
             throw new DataIntegrityViolationException(THROWING_EXCEPTION_FOR_DEMOING_ROLLBACK);
         }
-        if (opt.isPresent())
+        if (opt.isPresent()) {
             return opt.get();
-        else
-            return null;
+        }
+        return null;
     }
 }
